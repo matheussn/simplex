ddacabb8ef796c03c0aa76ddbc14845caac238d9
Matheus Santiago Neto <matheusneto23@gmail.com>
2019-05-28 20:23:13 -0300

    Update funcs.js

--- funcs.js
+++ funcs.js
@@ -32,3 +32,23 @@ module.exports = {
   getArrayBase: getArrayBase,
   getArrayRest: getArrayRest
 }
+
+/*
+
+function possuiNegativo(v) {
+    return v < 0
+}
+
+function maximize(obj) {
+  var vals = Object.keys(obj.z).map(function(key) {
+    return obj.z[key];
+  });
+  console.log(vals.some(possuiNegativo))
+}
+
+
+maximize({
+    z: { z: 1, x1: -5, x2: -4, s1: 0, s2: 0, s3: 0, s4: 0, Sol: 0},
+    s1: { z: 0, x1: 6, x2: 4, s1: 1, s2: 0, s3: 0, s4: 0, Sol: 24}
+});
+*/
